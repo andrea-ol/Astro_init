b76f28a020cb58b42e40988ccfee4feb3ed1eb51
andrea-ol <163352071+andrea-ol@users.noreply.github.com>
2026-03-26 16:19:27 -0500

    Refactor About and Profile pages to use BaseLayout component; create ProfileCard component for better structure and styling; update global CSS with new styles and variables for improved design and responsiveness.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('.menu');
+const navLinks = document.getElementById('main-menu');
 
 menu?.addEventListener('click', () => {
   const isExpanded = menu.getAttribute('aria-expanded') === 'true';
   menu.setAttribute('aria-expanded', `${!isExpanded}`);
+  navLinks?.classList.toggle('is-open', !isExpanded);
 });
